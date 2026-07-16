1d6c32cebcc9285ea91ea5fc8bc5eb89551e2c90
Leonardo Carvalho <leonardocarvalhosantos14@gmail.com>
2026-07-16 16:55:18 -0300

    feat: adiciona a classe funcionario

---
+++ src/main/java/funcionarios/model/Funcionario.java
@@ -0,0 +1,30 @@
+package funcionarios.model;
+
+public class Funcionario {
+    private final Long id;
+    private String nome;
+    private String departamento;
+
+    public Funcionario(Long id, String nome, String departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.departamento = departamento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public Funcionario setDepartamento(String departamento) {
+        this.departamento = departamento;
+        return this;
+    }
+}
