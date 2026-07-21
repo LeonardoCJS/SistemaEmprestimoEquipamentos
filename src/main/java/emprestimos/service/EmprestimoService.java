d577b2aff700582c6b9d2b1ae26e65192184092e
Leonardo Carvalho <leonardocarvalhosantos14@gmail.com>
2026-07-21 15:28:19 -0300

    feat: implementa registrarEmprestimo em EmprestimoService

---
+++ src/main/java/emprestimos/service/EmprestimoService.java
@@ -0,0 +1,35 @@
+package emprestimos.service;
+
+import emprestimos.model.Emprestimo;
+import emprestimos.repository.EmprestimoRepository;
+import equipamentos.model.Equipamento;
+import equipamentos.repository.EquipamentoRepository;
+import exceptions.LimiteEmprestimoExcedidoException;
+import funcionarios.model.Funcionario;
+import funcionarios.repository.FuncionarioRepository;
+
+
+public class EmprestimoService {
+    private final EmprestimoRepository emprestimoRepository;
+    private final EquipamentoRepository equipamentoRepository;
+    private final FuncionarioRepository funcionarioRepository;
+
+    public EmprestimoService(EmprestimoRepository emprestimoRepository,  EquipamentoRepository equipamentoRepository, FuncionarioRepository funcionarioRepository) {
+        this.emprestimoRepository = emprestimoRepository;
+        this.equipamentoRepository = equipamentoRepository;
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    public String registrarEmprestimo(Long idFuncionario, Long idEquipamento) {
+        Funcionario funcionario = funcionarioRepository.buscarPorId(idFuncionario);
+        Equipamento equipamento = equipamentoRepository.buscarPorId(idEquipamento);
+        long emprestimosAtivos = emprestimoRepository.listarAtivos().stream()
+                .filter(empres -> empres.getFuncionario().getId().equals(funcionario.getId()))
+                .count();
+        if (emprestimosAtivos >= 3) {
+            throw new LimiteEmprestimoExcedidoException("O funcionario ja tem 3 emprestimos ativos!");
+        }
+        emprestimoRepository.salvar(new Emprestimo(emprestimoRepository.proximoId(), equipamento, funcionario));
+        return "Emprestimo salvo com sucesso!";
+    }
+}
